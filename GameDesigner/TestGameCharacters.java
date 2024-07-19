b6587184c3880507037883d69bb7755381d8b953
Ethan <lyg23@txstate.edu>
2024-07-18 21:12:12 -0500

    new file:   GameDesigner/TestGameCharacters.java

---
+++ GameDesigner/TestGameCharacters.java
@@ -0,0 +1,49 @@
+package GameDesigner;
+
+public class TestGameCharacters {
+    public static void main(String[] args) {
+        // Test Case 1: Wizard Actions
+        Wizard wizard = new Wizard("Gandalf", 50, 100, 200);
+        wizard.castSpell();
+        wizard.attack();
+        wizard.fly();
+        wizard.teleport(10, 20);
+        wizard.land();
+
+        // Test Case 2: Mermaid Actions
+        Mermaid mermaid = new Mermaid("Ariel", 30, 80, 50);
+        mermaid.sing();
+        mermaid.swim();
+        mermaid.defend();
+        mermaid.stopSwimming();
+
+        // Test Case 3: Superhero Actions
+        Superhero superhero = new Superhero("Superman", 70, 120, "Super Strength");
+        superhero.saveTheDay();
+        superhero.attack();
+        superhero.fly();
+        superhero.teleport(5, 15);
+        superhero.land();
+        superhero.swim();
+        superhero.stopSwimming();
+
+        // Test Case 4: Polymorphism and Interface Application
+        GameCharacter[] characters = {wizard, mermaid, superhero};
+        for (GameCharacter character : characters) {
+            character.attack();
+            character.defend();
+
+            if (character instanceof Flyable) {
+                ((Flyable) character).fly();
+                ((Flyable) character).land();
+            }
+            if (character instanceof Swimmable) {
+                ((Swimmable) character).swim();
+                ((Swimmable) character).stopSwimming();
+            }
+            if (character instanceof Teleportable) {
+                ((Teleportable) character).teleport(1, 1);
+            }
+        }
+    }
+}
